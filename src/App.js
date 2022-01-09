914455782fb34e8e86ae3cf4bf157d83752c3e07
nickpazfernande <nickpazfernandez@gmail.com>
2022-01-08 23:34:44 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/teams" element={<InfoTeam />} />
